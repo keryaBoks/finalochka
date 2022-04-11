813798d5865d66603a689ca8159770bd972e0203
keryaBoks <kiryl@wsb-203-74b.local>
2022-04-11 14:14:53 +0300

    feat create connections and tables

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const sequelize = require('./db');
+require('dotenv').config();
+
+const app = express();
+
 const PORT = process.env.PORT || 5001
 
-app.listen(PORT, ()=> console.log(`server has started on Port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`server has started on Port ${PORT}`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
